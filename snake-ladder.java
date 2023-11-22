1b08df58f86bca8371c4f82bbb96e9c7773c957c
prateekch33 <prateekchandra27@outlook.com>
2023-11-22 23:54:30 +0530

    Report Output

--- snake-ladder.java
+++ snake-ladder.java
@@ -1,35 +1,43 @@
 import java.util.*;
 
 class SnakeLadder {
-    int pos,dice;
+    int pos, dice, countRoll;
+
     SnakeLadder() {
-        pos=0;
+        pos = 0;
     }
+
     public void dice() {
-        dice=(int)(Math.random()*6)+1;
+        dice = (int) (Math.random() * 6) + 1;
+        countRoll++;
         System.out.println("Dice Rolled");
     }
+
     public void checker() {
-        int state=(int)(Math.random()*10)%3;
-        if(state==0) {
-            pos+=dice;
-            if(pos>100) {
-                pos-=dice;
+        int state = (int) (Math.random() * 10) % 3;
+        if (state == 0) {
+            pos += dice;
+            if (pos > 100) {
+                pos -= dice;
             }
-        } else if(pos==1) {
-            pos-=dice;
+        } else if (pos == 1) {
+            pos -= dice;
         }
     }
+
     public void playAutomatically() {
         while(pos!=100) {
+            dice();
             checker();
             if(pos<0) pos=0;
+            System.out.printf("Position: %d",pos);
         }
     }
+
     public static void main(String[] args) {
-        SnakeLadder ob=new SnakeLadder();
-        ob.dice();
-        ob.checker();
+        SnakeLadder ob = new SnakeLadder();
+        // ob.dice();
+        // ob.checker();
         ob.playAutomatically();
     }
 }
