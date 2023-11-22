134b11ec81af657e3b5859335412e8b92f7104c9
prateekch33 <prateekchandra27@outlook.com>
2023-11-22 23:01:25 +0530

    Game initiated

---
+++ snake-ladder.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class SnakeLadder {
+    int pos;
+    SnakeLadder() {
+        pos=0;
+    }
+    public static void main(String[] args) {
+        SnakeLadder ob=new SnakeLadder();
+    }
+}
